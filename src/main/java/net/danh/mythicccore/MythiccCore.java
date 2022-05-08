22f3ae994ba2816508ac38d3f80e05ee950497bf
VoChiDanh <vochidanh.hs.c@gmail.com>
2022-05-08 21:06:41 +0700

    Improve SoulPoints System Some features can't use if can't found MMOItems.

--- src/main/java/net/danh/mythicccore/MythiccCore.java
+++ src/main/java/net/danh/mythicccore/MythiccCore.java
@@ -5,13 +5,17 @@ import net.danh.mythicccore.Commands.OpenRegeneratorGui;
 import net.danh.mythicccore.Commands.OpenUpgradeGui;
 import net.danh.mythicccore.Commands.SoulPoints;
 import net.danh.mythicccore.Compatible.Placeholder;
+import net.danh.mythicccore.Data.Storage;
 import net.danh.mythicccore.Events.Death;
 import net.danh.mythicccore.Events.Inventory;
 import net.danh.mythicccore.Events.Join;
 import net.danh.mythicccore.Events.Quit;
+import net.danh.mythicccore.NMS.NMSAssistant;
+import net.danh.mythicccore.Utils.Chat;
 import net.danh.mythicccore.Utils.Resources;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.logging.Level;
 
@@ -33,25 +37,51 @@ public final class MythiccCore extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(new Inventory(), this);
+        if (getServer().getPluginManager().getPlugin("MMOItems") != null) {
+            NMSAssistant nmsAssistant = new NMSAssistant();
+            getLogger().log(Level.INFO, Chat.colorize("&6Server version:&3 " + nmsAssistant.getNMSVersion()));
+            getServer().getPluginManager().registerEvents(new Inventory(), this);
+            new OpenRegeneratorGui(this);
+            new OpenUpgradeGui(this);
+            getLogger().log(Level.INFO, "Hooking into MMOItems");
+            getLogger().log(Level.INFO, Chat.colorize("&a✓&f Upgrade items features"));
+            getLogger().log(Level.INFO, Chat.colorize("&a✓&f Identify items features"));
+        } else {
+            getLogger().log(Level.INFO, "Can not found MMOItems");
+            getLogger().log(Level.INFO, Chat.colorize("&c✘&f Upgrade items features"));
+            getLogger().log(Level.INFO, Chat.colorize("&c✘&f Identify items features"));
+        }
+        getLogger().log(Level.INFO, "--------------------------------------------");
+        if (getServer().getPluginManager().getPlugin("PlaceholderAPI") != null) {
+            getLogger().log(Level.INFO, "Hooking into PlaceholderAPI");
+            getLogger().log(Level.INFO, Chat.colorize("&a✓&f Placeholder features"));
+            new Placeholder().register();
+        } else {
+            getLogger().log(Level.INFO, "Can not found PlaceholderAPI");
+            getLogger().log(Level.INFO, Chat.colorize("&c✘&f Placeholder features"));
+        }
+        getLogger().log(Level.INFO, "--------------------------------------------");
         getServer().getPluginManager().registerEvents(new Join(), this);
         getServer().getPluginManager().registerEvents(new Quit(), this);
         getServer().getPluginManager().registerEvents(new Death(), this);
-        new OpenRegeneratorGui(this);
         new MythiccCMD(this);
-        new OpenUpgradeGui(this);
         new SoulPoints(this);
-        if (getServer().getPluginManager().getPlugin("MMOItems") != null) {
-            getLogger().log(Level.INFO, "Hooking into MMOItems");
-        }
-        if (getServer().getPluginManager().getPlugin("MythicLib") != null) {
-            getLogger().log(Level.INFO, "Hooking into MythicLib");
-        }
-        if (getServer().getPluginManager().getPlugin("PlaceholderAPI") != null) {
-            new Placeholder().register();
-            getLogger().log(Level.INFO, "Hooking into PlaceholderAPI");
-        }
+        getLogger().log(Level.INFO, Chat.colorize("&a✓&f SoulPoints features"));
         Resources.createfiles();
+        (new BukkitRunnable() {
+            public void run() {
+                for (Player p : getServer().getOnlinePlayers()) {
+                    net.danh.mythicccore.Data.SoulPoints.addSoulPoints(p, 1);
+                }
+            }
+        }).runTaskTimer(this, 1800 * 20L, 1800 * 20L);
+        (new BukkitRunnable() {
+            public void run() {
+                for (Player p : getServer().getOnlinePlayers()) {
+                    Storage.savePlayerData(p);
+                }
+            }
+        }).runTaskTimer(this, 3600 * 20L, 3600 * 20L);
     }
 
     @Override
